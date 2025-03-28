99d842966703a654740ec280167e4909540810f6
Jamilasullivan <sullivan-al-kadhomiyj@cardiff.ac.uk>
2025-03-28 12:39:12 +0000

    adding first few useful commands

---
+++ useful_general_commands.R
@@ -0,0 +1,4 @@
+asb(x) # gives an absolute value in both directions. Useful for Log2fold change.
+str(x) # gives the structure of a data set
+head(x) # gives the first 6 rows of a data set
+tail(x) # gives the last 6 rows of a data set
